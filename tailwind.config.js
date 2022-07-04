febc54dd7c311dacbdb75299c6d2dc8d96e48563
NazrulIslam314 <nazrulislam3144@gmail.com>
2022-07-04 18:03:09 +0600

    Try some Deferent

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./**.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Arima', 'cursive'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['Gulzar', 'serif'],
+      'display': ['Oswald'],
+      'body': ['"Open Sans"'],
+    },
+    extend: {
+      colors: {
+        'forst-white': '#D4F7EC',
+        'forst-green': '#448D76',
+        'forst-yellow': '#FB8604',
+        'forst-blue': '#2E2FE3',
+        'forst-drak': '#090934',
+      },
+    },
   },
   plugins: [],
 }
