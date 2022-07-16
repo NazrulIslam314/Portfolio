ff002cdde466a07fbfe5a5c89765bd18aa30f819
NazrulIslam314 <nazrulislam3144@gmail.com>
2022-07-16 19:21:47 +0600

    centered

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,24 @@
-$(document).ready(function () {
-    $(".nav-toggler").each(function (_, navToggler) {
-      var target = $(navToggler).data("target");
-      $(navToggler).on("click", function () {
-        $(target).animate({
-          height: "toggle",
-        });
-      });
-    });
-  });
- const sentBtn = document.getElementById("sent");
- const showArea = document.getElementById("showArea");
- sentBtn.addEventListener("click", () => {
-  showArea.innerHTML = ("sent Success 🏅");
- } );
+// $(document).ready(function () {
+//     $(".nav-toggler").each(function (_, navToggler) {
+//       var target = $(navToggler).data("target");
+//       $(navToggler).on("click", function () {
+//         $(target).animate({
+//           height: "toggle",
+//         });
+//       });
+//     });
+//   });
+const navToggler = document.querySelector(".nav-toggler");
+const nav = document.querySelector("#navigation");
+navToggler.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
+const navitem = document.getElementsByTagName("span");
+navitem.addEventListener("click", () => {
+  nav.classList.remove("show");
+});
+const sentBtn = document.getElementById("sent");
+const showArea = document.getElementById("showArea");
+sentBtn.addEventListener("click", () => {
+  showArea.innerHTML = "sent Success 🏅";
+});
